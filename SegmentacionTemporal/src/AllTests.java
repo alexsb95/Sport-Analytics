d43b19b7b6a6234ab7a9010f7dd71595b19d42a6
alexsb95 <alexsb95@gmail.com>
2016-08-20 22:59:55 -0600

    Se agrego la funcionalidad de os trozos del sistema.

--- SegmentacionTemporal/src/AllTests.java
+++ SegmentacionTemporal/src/AllTests.java
@@ -1,6 +1,7 @@
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
+import org.junit.runners.Suite.SuiteClasset;
 
 @RunWith(Suite.class)
 @SuiteClasses({ CapturarVideoTest.class, ExtracionCapaHTest.class, TransformacionHSVTest.class })
---
+++ Sport-Analytics/src/SegmentacionTemporal/AllTests.java
@@ -0,0 +1,10 @@
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasset;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CapturarVideoTest.class, ExtracionCapaHTest.class, TransformacionHSVTest.class })
+public class AllTests {
+
+}
